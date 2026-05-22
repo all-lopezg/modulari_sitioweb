5dfcbfdfb5cb099925cc244e3c581b06381e13ca
Paloma <paloma.cabrillana@gmail.com>
2026-05-22 18:05:26 -0400

    Responsive página 'Nosotros'

--- app/nosotros/page.jsx
+++ app/nosotros/page.jsx
@@ -9,20 +9,22 @@ const merriweather = Merriweather({
 
 export default function AboutPage() {
     return (
-        <div className="flex mb-5 lg:mx-20 lg:gap-x-4">
+        <div className="flex flex-col justify-center items-center lg:items-stretch lg:flex-row mb-5 mx-5 lg:mx-20 gap-y-3 gap-x-4">
 
-            <div className="bg-celeste text-white rounded-lg w-[50%] text-center grid py-15 px-10">
+            <div className="bg-celeste text-white rounded-lg lg:w-[50%] text-center grid gap-y-4 lg:gap-y-0 py-8 lg:py-15 px-8 lg:px-10">
                 <h2 className="text-5xl lg:text-6xl">Nosotros</h2>
-                <p className={`${merriweather.className} lg:text-[25px]`}>Somos 2 hermanos apasionados por el diseño y la arquitectura.</p>
-                <p className="lg:text-[22px]">Así nace Modulari: una marca de mobiliario de lujo especializada en sofás modulares y arriendo de piezas exclusivas para eventos con identidad y estilo.</p>
+                <p className={`${merriweather.className} text-xl lg:text-[25px]`}>Somos 2 hermanos apasionados por el diseño y la arquitectura.</p>
+                <p className="text-lg lg:text-[22px]">Así nace Modulari: una marca de mobiliario de lujo especializada en sofás modulares y arriendo de piezas exclusivas para eventos con identidad y estilo.</p>
             </div>
 
-            <Image
-                src={'/imgs/nosotros/nosotros.png'}
-                alt="modulari"
-                width={920} height={1160}
-                className="aspect-square object-cover w-[50%] h-[75vh] rounded-lg"
-            />
+            <div className="overflow-hidden rounded-lg lg:w-[50%] lg:h-[75vh]">
+                <Image
+                    src={'/imgs/nosotros/nosotros.png'}
+                    alt="modulari"
+                    width={920} height={1160}
+                    className="aspect-square object-cover w-full h-full scale-130 lg:scale-100"
+                />
+            </div>
 
         </div>
     )
