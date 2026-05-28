acc5d72ccefe775f5e910ce6d8840e44f7d90113
Paloma <paloma.cabrillana@gmail.com>
2026-05-28 16:45:25 -0400

    Sección 'Vitrina Virtual' terminada

--- app/ui/inicio/catalogo-card.jsx
+++ app/ui/inicio/catalogo-card.jsx
@@ -2,15 +2,15 @@ import Image from "next/image"
 
 export default function CatalogoCard({ img, name }) {
     return (
-        <div className="w-fit grid gap-y-3 bg-white p-4 rounded-md">
+        <div className="grid gap-y-3 w-fit bg-white p-4 rounded-md shadow-[0_0_12px_2px_rgba(0,0,0,0.1)]">
             <Image 
                 src={img}
                 alt="modulari"
                 width={300} height={300}
-                className="rounded-md"
+                className="rounded-md w-74.75 sm:w-75"
             />
-            <p className="text-xl">{name}</p>
-            <button className="text-xl bg-celeste text-white flex items-center ml-auto p-2 rounded-md">Más información</button>
+            <p className="text-[22px]">{name}</p>
+            <button className="text-lg bg-celeste text-white flex items-center ml-auto px-2 py-1.5 rounded-md">Más información</button>
         </div>
     )
 }
--- app/ui/inicio/catologo.jsx
+++ app/ui/inicio/catologo.jsx
@@ -1,58 +1,222 @@
 "use client"
 import CatalogoCard from "./catalogo-card"
 import { catalogo } from "@/app/lib/catalogo"
-import { useState, useRef } from "react"
+import { useState, useRef, useEffect, useCallback } from "react"
+import { ChevronRight, ChevronLeft } from 'lucide-react';
 
 export default function Catalogo() {
 
-    const [currentSlide, setCurrentSlide] = useState(0);
-    const carouselRef = useRef(null);
+    const carouselRef = useRef(null)
+    const cardRef = useRef(null)
+    const cardRefs = useRef([])
 
-    // Detectar scroll y actualizar bolitas
-    const handleScroll = () => {
+    {/* Transparenta las cards que no se vean al 100% en el carousel */ }
+    const handleCardOpacity = useCallback(() => {
 
-        const container = carouselRef.current; // div del carousel
-        if (!container) return;
+        const carousel = carouselRef.current
+        if (!carousel) return
 
-        const scrollLeft = container.scrollLeft; // cuantos pixeles se movió
-        const itemWidth = container.children[0]?.offsetWidth + 16; // ancho de la card + gap
-        const index = Math.round(scrollLeft / itemWidth);
+        const cardWidth = cardRef.current.offsetWidth + 20
 
-        setCurrentSlide(Math.min(index, catalogo.length - 1));
+        cardRefs.current.forEach((card, index) => {
+            if (!card) return
 
-        console.log(itemWidth, scrollLeft)
-    };
+            const cardLeft = index * cardWidth
+            const cardRight = cardLeft + cardWidth
+
+            const visible = cardLeft >= carousel.scrollLeft && cardRight <= carousel.scrollLeft + carousel.offsetWidth
+            card.style.opacity = visible ? '1' : '0.5'
+        })
+    }, [])
+
+    useEffect(() => {
+
+        handleCardOpacity()
+        const observer = new ResizeObserver(handleCardOpacity)
+
+        observer.observe(carouselRef.current)
+
+        return () => observer.disconnect()
+
+    }, [handleCardOpacity])
+
+    {/* Crea una Dot Pagination en base a la cantidad de slides */}
+    const [activeSlide, setActiveSlide] = useState(0)
+    const [totalSlides, setTotalSlides] = useState(catalogo.length)
+
+    const handleDotPagination = useCallback(() => {
+
+        const carousel = carouselRef.current
+        const card = cardRef.current
+        if (!carousel || !card) return
+
+        const cardWidth = card.offsetWidth + 20
+        const cardsVisible = Math.trunc(carousel.offsetWidth / cardWidth)
+
+        const index = Math.round(carousel.scrollLeft / (cardWidth * cardsVisible))
+
+        setTotalSlides(catalogo.length / cardsVisible)
+        setActiveSlide(index)
+    }, [])
+
+    useEffect(() => {
+
+        handleDotPagination()
+        const observer = new ResizeObserver(handleDotPagination)
+
+        observer.observe(carouselRef.current)
+        return () => observer.disconnect()
+
+    }, [handleDotPagination])
+
+    {/* Calcula cuantos px se debe mover el carousel */ }
+    const [move, setMove] = useState(0)
+    const [paddingInline, setPaddingInline] = useState(20)
+
+    useEffect(() => {
+
+        if (window.innerWidth < 640) return // no se ejecuta en mobiles
+
+        const calculateMove = () => {
+
+            if (!carouselRef.current || !cardRef.current) return
+
+            const carouselWidth = carouselRef.current.offsetWidth
+            const cardWidth = cardRef.current.offsetWidth + 20 // gap
+
+            const cardsVisible = Math.trunc(carouselWidth / cardWidth)
+
+            setMove(cardWidth * cardsVisible)
+            setPaddingInline((carouselWidth % cardWidth) / 2)
+        }
+
+        calculateMove()
+
+        const observer = new ResizeObserver(() => {
+            calculateMove()
+            carouselRef.current.scrollLeft = 0
+        })
+        observer.observe(carouselRef.current)
+
+        return () => observer.disconnect()
+    }, [])
+
+    {/* Scrollea los px calculados antes en mobiles */ }
+    useEffect(() => {
+
+        if (window.innerWidth < 640) return // no se ejecuta en mobiles
+
+        const carousel = carouselRef.current
+        if (!carousel) return
+
+        let startX = 0
+
+        // posición horizontal del dedo cuando toca la pantalla por primera vez (en px)
+        const onTouchStart = (e) => {
+            startX = e.touches[0].clientX
+        }
+
+        const onTouchEnd = (e) => {
+            // e.changedTouches[0].clientX -> posición horizontal del dedo cuando sale de la pantalla (en px)
+            const swipe = startX - e.changedTouches[0].clientX
+            // negativo -> izquierda ; positivo -> derecha
+
+            if (Math.abs(swipe) < 30) return // ignora taps accidentales
+
+            carousel.scrollBy({
+                left: swipe > 0 ? move : -move,
+                behavior: 'smooth'
+            })
+        }
+
+        carousel.addEventListener('touchstart', onTouchStart) // cuando el dedo toca carousel, se ejecuta la función dada
+        carousel.addEventListener('touchend', onTouchEnd) // cuando el dedo se levanta, se ejecuta la función dada
+
+        // Cleanup; se ejecuta al desmontar el componente
+        // Evita que el evento siga registrado en memoria
+        return () => {
+            carousel.removeEventListener('touchstart', onTouchStart)
+            carousel.removeEventListener('touchend', onTouchEnd)
+        }
+    }, [move])
+
+    {/* Avanza los px calculados antes en desktop */ }
+    const handleMove = (direction) => {
+
+        if (window.innerWidth < 640) return // no se ejecuta en mobiles
+
+        const carousel = carouselRef.current
+        if (!carousel) return
+
+        carousel.scrollBy({
+            left: direction === 'right' ? move : -move,
+            behavior: 'smooth'
+        })
+    }
+
+    {/* Ejecuta las dos funciones al mismo tiempo al hacer scroll */}
+    const onScroll = () => {
+        handleCardOpacity()
+        handleDotPagination()
+    }
 
     return (
-        <div className="bg-[#F6F6F6] grid justify-center py-10 mx-2 rounded-md w-fit overflow-hidden">
+        <div className="mx-5 pb-3 lg:mx-10 overflow-hidden rounded-lg lg:relative">
+            <h2 className="text-center my-10 text-5xl lg:text-6xl">Vitrina virtual</h2>
 
-            <h2 className="text-center text-5xl mb-8">Revisa nuestra <br className="md:hidden" /> colección</h2>
-
+            {/* Carrusel */}
             <div
                 ref={carouselRef}
-                onScroll={handleScroll}
-                className="flex gap-x-4 overflow-x-scroll w-full">
-                {catalogo.map((mueble, index) => (
-                    <div key={index} className="shrink-0">
-                        <CatalogoCard
-                            img={mueble.src}
-                            name={mueble.name}
-                        />
+                onScroll={onScroll}
+                style={{ paddingInline: `${paddingInline}px` }}
+                className="overflow-x-scroll sm:overflow-x-hidden flex snap-x snap-mandatory scroll-smooth hide-scrollbar gap-x-5"
+            >
+                {catalogo.map((item, index) => (
+                    <div
+                        key={index}
+                        ref={el => {
+                            cardRefs.current[index] = el
+                            if (index === 0) cardRef.current = el
+                        }}
+                        className="flex shrink-0 transition-opacity w-fit snap-center sm:snap-align-none my-3"
+                    >
+                        <CatalogoCard img={item.src} name={item.name} />
                     </div>
                 ))}
             </div>
 
-            <div className="flex justify-center gap-2 mt-4">
-                {catalogo.map((_, index) => (
-                    <button
+            <div className="flex justify-center items-center gap-1.5 mt-3">
+                {Array.from({ length: Math.ceil(totalSlides) }).map((_, index) => (
+                    <div
                         key={index}
-                        className={`h-2 rounded-full transition-all ${index === currentSlide
-                            ? 'bg-blue-500 w-8'
-                            : 'bg-gray-300 w-2'
+                        className={`transition-all duration-300 rounded-full h-2
+                            ${activeSlide === index
+                                ? 'w-6 rounded bg-celeste'
+                                : 'w-2 bg-gray-300'
                             }`}
                     />
                 ))}
             </div>
+
+            <div className="hidden sm:flex justify-center mt-5">
+
+                <button
+                    type="button"
+                    onClick={() => handleMove('left')}
+                    className="cursor-pointer hover:text-celeste"
+                >
+                    <ChevronLeft size={45} />
+                </button>
+
+                <button
+                    type="button"
+                    onClick={() => handleMove('right')}
+                    className="cursor-pointer hover:text-celeste"
+                >
+                    <ChevronRight size={45} />
+                </button>
+            </div>
+
         </div>
-    )
+    );
 }
